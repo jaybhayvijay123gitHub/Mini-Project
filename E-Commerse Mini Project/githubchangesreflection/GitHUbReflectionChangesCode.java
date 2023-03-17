66430d4ca41cbe7a232a8300c86128c2a9f15299
Your Name <you@example.com>
2023-03-17 20:03:08 +0530

    that folder will be delete after this commit

--- E-Commerse Mini Project/githubchangesreflection/GitHUbReflectionChangesCode.java
+++
@@ -1,8 +0,0 @@
-package com.githubchangesreflection;
-
-public class GitHUbReflectionChangesCode {
-	public static void main(String[] args) {
-		System.out.println("This is trial of putting our writeup code into the github repository !");
-
-	}
-}
