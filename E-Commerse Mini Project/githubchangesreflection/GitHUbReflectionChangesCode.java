501bdff50ca7399313733d80832cd9790a56a419
Your Name <you@example.com>
2023-03-17 20:00:57 +0530

    my commit

---
+++ E-Commerse Mini Project/githubchangesreflection/GitHUbReflectionChangesCode.java
@@ -0,0 +1,8 @@
+package com.githubchangesreflection;
+
+public class GitHUbReflectionChangesCode {
+	public static void main(String[] args) {
+		System.out.println("This is trial of putting our writeup code into the github repository !");
+
+	}
+}
