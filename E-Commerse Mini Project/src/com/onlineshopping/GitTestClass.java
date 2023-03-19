9e8879682d4857d61ddbfc902d3d34cd5156bd1d
Your Name <you@example.com>
2023-03-19 18:16:40 +0530

    Adding new Class

---
+++ E-Commerse Mini Project/src/com/onlineshopping/GitTestClass.java
@@ -0,0 +1,7 @@
+package com.onlineshopping;
+
+public class GitTestClass {
+	public static void main(String[] args) {
+		System.out.println("This is the addition of class to understand the git changes ! ");
+	}
+}
